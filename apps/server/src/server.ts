05ef9c9b0471d123b9f5963a012ec59dcc6d1e4e
kinkenkan298 <akin@programmer.net>
2026-02-08 16:51:20 +0700

    feat: set up cors express

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,15 +1,16 @@
 import express from "express";
+import cors from "cors";
 import { errorHandler } from "@/middleware/error-handler";
 
 const app = express();
 
 app.use(express.json());
-
 app.use(
   express.urlencoded({
     extended: true,
   }),
 );
+app.use(cors());
 
 app.use(errorHandler);
 
